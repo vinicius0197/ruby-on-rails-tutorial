75f6da0af99a65f6d929234483436d012f88a85f
Vinicius Costa <viniciuscosta0197@gmail.com>
2019-08-21 22:18:50 -0300

    Implement advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
+    post users_path, params: { user: { name: "",
                                  email: "user@invalid",
                                  password: "foo",
                                  password_confirmation: "bar" } }
@@ -17,7 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
 
     assert_difference 'User.count' do
-      post users_path, params: { user: { name: "Example user",
+    post users_path, params: { user: { name: "Example user",
                                  email: "user@example.com",
                                  password: "password",
                                  password_confirmation: "password"}}
